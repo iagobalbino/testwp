a61b69055d8b0d13e68899278d53aadbcc3c6c52
Iago Balbino <iago_nobre@hotmail.com>
2023-07-19 06:59:13 -0300

    Finished lesson 4-7

--- wp-content/themes/rest/footer.php
+++ wp-content/themes/rest/footer.php
@@ -1,15 +1,16 @@
-<footer>
-			<nav class="nav-footer">
-				<ul>
-				<li class="current_page_item"><a href="/testewp/">Menu</a></li>
-					<li><a href="/testewp/sobre/">Sobre</a></li>
-					<li><a href="/testewp/contato/">Contato</a></li>
-				</ul>
-			</nav>
+	<footer>
+				<nav class="nav-footer">
+					<ul>
+					<li class="current_page_item"><a href="/testewp/">Menu</a></li>
+						<li><a href="/testewp/sobre/">Sobre</a></li>
+						<li><a href="/testewp/contato/">Contato</a></li>
+					</ul>
+				</nav>
 
-			<p>Rest © 2015. Alguns direitos reservados.</p>
-		</footer>
+				<p>Rest © 2015. Alguns direitos reservados.</p>
+	</footer>
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
+
 	</body>
 </html>
